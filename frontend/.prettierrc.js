0380882beda14d74621f796c950ec9df3514b8b5
rin-cast-9 <117419794+rin-cast-9@users.noreply.github.com>
2025-02-01 03:27:38 +0800

    chore: add prettier and eslint configs for frontend

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    singleQuote: false,
+    trailingComma: 'all',
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    printWidth: 256
+};
