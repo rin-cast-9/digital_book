4370597fcc305f3519d258e3ec7e53f25a56a82f
rin-cast-9 <117419794+rin-cast-9@users.noreply.github.com>
2025-02-18 12:58:51 +0800

    feat: add operator local key storage form

--- frontend/src/components/UI/KeyVerification.tsx
+++ frontend/src/components/UI/KeyVerification.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import LabeledInput from "./LabeledInput";
-import { BookManagerContract, DEFAULT_ADMIN_ROLE } from "../../constants/blockchain";
+import { BookManagerContract, DEFAULT_ADMIN_ROLE, OPERATOR_ROLE } from "../../constants/blockchain";
 import { InputRole } from "../../constants/InputRole";
 import { useAdmin } from "../../contexts/AdminContext";
 
 
 const KeyVerification = () => {
     const { adminKey, setAdminKey, setIsAdminVerified, onSubmitAdmin, onRevokeAdmin } = useAdmin();
+    const [operatorKey, setOperatorKey] = useState("");
 
     useEffect(() => {
         if (sessionStorage.getItem("adminKey")) {
@@ -16,6 +17,19 @@ const KeyVerification = () => {
 
     const onVerifyAdmin = async (key: string): Promise<boolean> => {
         return await BookManagerContract.hasRole(DEFAULT_ADMIN_ROLE, key);
+    }
+
+    const onSubmitOperator = () => {
+        sessionStorage.setItem("operatorKey", operatorKey);
+    };
+
+    const onRevokeOperator = () => {
+        sessionStorage.removeItem("operatorKey");
+        setOperatorKey("");
+    };
+
+    const onVerifyOperator = async (key: string) => {
+        return await BookManagerContract.hasRole(OPERATOR_ROLE, key);
     }
 
     return (
@@ -29,6 +43,18 @@ const KeyVerification = () => {
                 onVerify={onVerifyAdmin}
                 disableOnSuccess={true}
                 persistStorageKey="adminKey"
+                inputRole={InputRole.VERIFIER}
+            />
+
+            <LabeledInput
+                label="Operator key"
+                value={operatorKey}
+                onChange={setOperatorKey}
+                onSubmit={onSubmitOperator}
+                onRevoke={onRevokeOperator}
+                onVerify={onVerifyOperator}
+                disableOnSuccess={true}
+                persistStorageKey="operatorKey"
                 inputRole={InputRole.VERIFIER}
             />
         </>
