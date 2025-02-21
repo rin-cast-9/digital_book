26eb617c5acbfdc9ee46ace4f8f56f8c327bf590
rin-cast-9 <117419794+rin-cast-9@users.noreply.github.com>
2025-02-22 05:15:45 +0800

    feat: implement pushing a book to the blockchain

--- frontend/src/components/pages/AddBook.tsx
+++ frontend/src/components/pages/AddBook.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AuthorsInput from "../UI/AuthorsInput"
+import { BookManagerContract, provider } from "../../constants/blockchain";
 
 
 const AddBook = () => {
@@ -9,8 +10,46 @@ const AddBook = () => {
     const [yearPublished, setYearPublished] = useState<number>();
     const [authors, setAuthors] = useState<string[]>([""]);
 
+    const handleSubmit = async () => {
+        const omittedLastEmptyAuthors = authors.slice(0, -1);
+
+        const operatorAddress = sessionStorage.getItem("operatorKey");
+        if (!operatorAddress) {
+            throw new Error("Unauthorized access");
+        }
+
+        const operatorSigner = await provider.getSigner(operatorAddress);
+        
+        /**
+         * uint16 _yearPublished,
+         * string memory _name,
+         * string memory _publisher,
+         * string memory _publisherCity,
+         * string[] memory _authors
+         */
+        return BookManagerContract.connect(operatorSigner).addBook(
+            yearPublished,
+            title,
+            publisher,
+            publisherCity,
+            omittedLastEmptyAuthors
+        ).then(() => { })
+        .catch((error: Error) => {
+            console.error(`An error occurred during book submision: ${error}`);
+        })
+    }
+
     return (
         <>
+            <h2 className="mb-3">
+                Book
+            </h2>
+            <button
+                className="btn btn-outline-primary mb-4"
+                onClick={handleSubmit}
+            >
+                Submit
+            </button>
             <div className="sticky-top" style={{ minHeight: "600px" }}>
                 <div className="form-group">
                     <div className="mb-3">
@@ -76,8 +115,8 @@ const AddBook = () => {
                             className="form-control"
                             id="inputYear"
                             placeholder="Year published"
-                            value={yearPublished}
-                            onChange={(e) => setYearPublished(e.target.valueAsNumber)}
+                            value={yearPublished ?? ""}
+                            onChange={(e) => setYearPublished(e.target.valueAsNumber ?? 0)}
                         />
                     </div>
 
